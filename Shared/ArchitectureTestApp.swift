2f36b7a6bc0ed3e7f9674a19d0363b041e7ed4a6
Emmanuel Garsd <garsdle@gmail.com>
2021-04-21 14:50:12 -0700

    Initial Commit

---
+++ Shared/ArchitectureTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureTestApp.swift
+//  Shared
+//
+//  Created by Emmanuel Garsd on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
