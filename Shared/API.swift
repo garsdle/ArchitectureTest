7d324273a6c2358e24bc7e7cd1bf61b3a0d221b6
Emmanuel Garsd <garsdle@gmail.com>
2021-07-14 06:01:35 -0700

    Separates services

--- Shared/API.swift
+++ Shared/API.swift
@@ -45,6 +45,42 @@ extension API {
 
 enum APIError: Error { }
 
+
+struct NestingFlight: Identifiable, Equatable {
+    let id: UUID
+    var name: String { id.uuidString }
+    var tickets: [Ticket]
+
+    static var mock: NestingFlight {
+        NestingFlight(id: .init(), tickets: [.mock, .mock])
+    }
+}
+
+struct Flight: Identifiable, Equatable {
+    let id: UUID
+    var name: String { id.uuidString }
+
+    static var mock: Flight {
+        Flight(id: .init())
+    }
+}
+
+struct Ticket: Identifiable, Equatable {
+    let id: UUID
+    let flightId: Flight.ID
+    var name: String
+
+    init(id: UUID, flightId: UUID) {
+        self.id = id
+        self.flightId = flightId
+        self.name = id.uuidString
+    }
+
+    static var mock: Ticket {
+        Ticket(id: .init(), flightId: .init())
+    }
+}
+
 class MockServer {
     var flights = [Flight.ID: Flight]() {
         didSet {
